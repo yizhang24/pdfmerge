8e29fb47470709d567c9f6467396f9f9d4b2155b
Yi Zhang <yi.zhang.yzfl@gmail.com>
2024-09-14 00:05:13 -0400

    Write code

--- main.cpp
+++ main.cpp
@@ -1,5 +1,33 @@
 #include <iostream>
+#include <cstring>
+#include <PDFWriter.h>
+#include <PDFParser.h>
+#include <InputFile.h>
 
-int main() {
-    std::cout << "Test" << "\n";
+int main(int argc, char *argv[]) {
+    for (int i = 0; i < argc; i++)
+    {
+        if (std::strcmp(argv[i], "-h") == 0) {
+            std::cout << "usage: pdfmerge [-h] file1 file2 ... output.pdf";
+            exit(0);
+        }
+        if (std::strcmp(argv[i], "-v") == 0)
+        {
+            std::cout << "1.0.0";
+            exit(0);
+        }
+    }
+
+    if (argc == 2) {
+        std::cout << "Please input at one input file and an output file";
+        exit(0);
+    }
+
+    PDFWriter writer;
+    writer.StartPDF(argv[argc - 1], ePDFVersion13);
+    for (int i = 1; i < argc; i++)
+    {
+        writer.AppendPDFPagesFromPDF(argv[i], PDFPageRange());
+    }
+    writer.EndPDF();
 }
